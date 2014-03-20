de70db83e294587215e3a8abfb94adab22b0ebc8
Khazov Andrey <khazov.andrew@gmail.com>
2014-03-21 00:49:48 +0400

    nothing works

---
+++ FirstTry/FirstTry/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace FirstTry
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreateXls = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CreateXls
+            // 
+            this.CreateXls.Location = new System.Drawing.Point(107, 151);
+            this.CreateXls.Name = "CreateXls";
+            this.CreateXls.Size = new System.Drawing.Size(75, 23);
+            this.CreateXls.TabIndex = 0;
+            this.CreateXls.Text = "button1";
+            this.CreateXls.UseVisualStyleBackColor = true;
+            this.CreateXls.Click += new System.EventHandler(this.CreateXls_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.CreateXls);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateXls;
+    }
+}
+
